be1a278984e532845b0b58eb79c573d7f411744a
zuqq <noahheld17@gmail.com>
2021-09-19 10:33:21 +0100

    Add `Parse` tests

---
+++ test/Coy/ParseSpec.hs
@@ -0,0 +1,54 @@
+module Coy.ParseSpec (spec) where
+
+import Data.Foldable (for_)
+import System.Directory (listDirectory)
+import System.FilePath ((</>), takeExtension)
+import Test.Hspec
+
+import qualified Data.ByteString as ByteString
+import qualified Data.Text.Encoding as Text.Encoding
+
+import Coy.Parse
+
+shouldParse :: FilePath -> Expectation
+shouldParse filePath = do
+    rawInput <- ByteString.readFile filePath
+    case Text.Encoding.decodeUtf8' rawInput of
+        Left e -> expectationFailure (show e)
+        Right s ->
+            case parse filePath s of
+                Left e -> expectationFailure (show e)
+                Right _ -> mempty
+
+shouldNotParse :: FilePath -> Expectation
+shouldNotParse filePath = do
+    rawInput <- ByteString.readFile filePath
+    case Text.Encoding.decodeUtf8' rawInput of
+        Left e -> expectationFailure (show e)
+        Right s ->
+            case parse filePath s of
+                Left _ -> mempty
+                Right _ -> expectationFailure "Unexpected successful parse."
+
+spec :: Spec
+spec = do
+    let goldenDirectory = "test/golden"
+
+    goldenFileNames <- runIO (getCoySourceFileNames goldenDirectory)
+
+    describe goldenDirectory (
+        for_ goldenFileNames (\fileName ->
+            it fileName (shouldParse (goldenDirectory </> fileName))))
+
+    let badDirectory = "test/parse/bad"
+
+    badFileNames <- runIO (getCoySourceFileNames badDirectory)
+
+    describe badDirectory (
+        for_ badFileNames (\fileName ->
+            it fileName (shouldNotParse (badDirectory </> fileName))))
+  where
+    isCoySourceFile fileName = takeExtension fileName == ".coy"
+
+    getCoySourceFileNames folder =
+        fmap (filter isCoySourceFile) (listDirectory folder)
--- test/Main.hs
+++ test/Main.hs
@@ -1,2 +1,1 @@
-main :: IO ()
-main = mempty
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
